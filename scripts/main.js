a05b88ecb5375b2fbd35d379899b6d35c1467f36
luhoballespin <ballespinfolktuc@gmail.com>
2025-03-12 15:58:30 -0300

    modificaciones

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,12 @@ document.addEventListener("scroll", () => {
 
 document.getElementById("theme-toggle").addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
+  const icon = this.querySelector("i");
+  if (document.body.classList.contains("dark-mode")) {
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+  } else {
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+  }
 });
